1d5fadb377b800c9ccf6e67148281ac4ca934890
Jonathan Bodrero <jbodrero@hotmail.com>
2025-06-21 18:57:59 -0600

    Madlib Instructions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Jonathan Bodrero
+// June 21, 2025
+//  Lab 4  Madlibs-1
+
+Console.Clear();
+//  Greeting / Instructions
+Console.WriteLine("This is Mad Libs. I will ask you for a variety of words of different types.");
+Console.WriteLine("When you are done providing the requested words I will build them \ninto a funny story for you. \nPlease enter the requested word types");
+
+//  Original story for MadLib
+string originalStory = "A vacation is when you take a trip to some (adjective) place with your (adjective) family. Usually, you go to some place that is near a/an (noun) or up on a/an (noun). A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun). I like to spend my time (verb ending in “ing”) or (verb ending in “ing”). When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body) My family is going to go to (place) and I will practice (verb ending in “ing”) Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number) hours every day all year making enough (plural noun) to pay for the vacation.";
+
